ea896ba84dda358d525071d475bedd11b2922f45
vagg <104461582+VaggelisProggraming@users.noreply.github.com>
2022-06-29 14:09:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 `use strict`;
 function refreshTime() {
   const timeDisplay = document.getElementById("time");
-  const dateString = new Date().toLocaleString();
+  const dateString = new Date().toLocaleTimeString();
   const formattedString = dateString.replace(", ", " - ");
   timeDisplay.textContent = formattedString;
 }
   setInterval(refreshTime, 1000);
+
+
+`use strict`;
+function refreshDate() {
+  const timeDisplay = document.getElementById("date");
+  const dateString = new Date().toLocaleDateString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshDate, 1000);
